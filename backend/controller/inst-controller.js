4149850821a905c4829630d35fb91f6f0ea9d96d
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2022-10-23 18:15:52 +0200

    addCourse getAllCourse

--- backend/controller/inst-controller.js
+++ backend/controller/inst-controller.js
@@ -29,4 +29,26 @@ const createCourse=async (req,res,next)=>{
         return res.status(201).json({course:course})
 }
 
-module.exports={createCourse};
+const getAllCourses = async (req, res) => {
+    const courses = await courseTable.find({}).sort({createdAt: -1})
+  
+    res.status(200).json(courses)
+  }
+// const getAllCourses=async(req,res,next)=>{
+    
+//     const{title,price,rating, instructor, totalHours, subject, description}=req.body
+//     let course;
+//     try{
+//       course=await courseTable.findAll();
+//     }
+//     catch(err){
+//        console.log(err);
+//     }
+//     // if(!course){
+//     //    return res.status(404).json({message:"no"})
+//     // }
+//     return res.status(200).json({course:course})
+// }
+
+module.exports={createCourse,
+getAllCourses};
--- backend/routes/inst-routes.js
+++ backend/routes/inst-routes.js
@@ -3,8 +3,9 @@ const instRouter=express.Router();
 const instTable=require("../models/Instructor");
 const courseTable=require("../models/Course");
 
-const {createCourse}=require("../controller/inst-controller");
+const {createCourse, getAllCourses}=require("../controller/inst-controller");
 
   
     instRouter.post("/",createCourse)
+    instRouter.get("/",getAllCourses)
     module.exports=instRouter;
